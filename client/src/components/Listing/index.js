7c8134e58d16f076a29ff75e7303d236c4bea342
Andreas Kaiafas <akaiafas526@gmail.com>
2022-04-27 11:29:38 -0400

    adjusted divs within home.js, cards on home page properly spaced

--- client/src/components/Listing/index.js
+++ client/src/components/Listing/index.js
@@ -1,9 +1,23 @@
 import React from "react";
-import "./listing.css"
+import "./listing.css";
 import Category from "../Category";
 import JobCard from "../JobCard";
 
 export default function Listing() {
+  // const [addJob, { error }] = useMutation(ADD_JOB, {
+  //   update(cache, { data: { addJob } }) {
+  //     try {
+  //       const { jobs } = cache.readQuery({ query: QUERY_ALL_JOBS });
+
+  //       cache.writeQuery({
+  //         query: QUERY__ALL_JOBS,
+  //         data: { jobs: [addJob, ...jobs] },
+  //       });
+  //     } catch (e) {
+  //       console.error(e);
+  //     }
+  //   },
+  // });
   const displayCard = [];
   for (let i = 0; i < 5; i++) {
     displayCard.push(<JobCard key={i} />);
@@ -11,9 +25,21 @@ export default function Listing() {
   return (
     <div>
       <Category />
-
-      <div className="display-card">{displayCard}</div>
-      <JobCard />
+      <div className="row justify-content-center">
+        {displayCard}
+      </div>
+      <div className="row justify-content-center">
+        {displayCard}
+      </div>
+      <div className="row justify-content-center">
+        {displayCard}
+      </div>
+      <div className="row justify-content-center">
+        {displayCard}
+      </div>
+      <div className="row justify-content-center">
+        {displayCard}
+      </div>
     </div>
   );
 }
