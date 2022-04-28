ea7761cc8d636af4b9dcb3d91213c70798a1ba68
Jamehzz <probableeme@gmail.com>
2022-04-28 10:55:10 -0400

    added styling and responsiveness to profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -12,9 +12,8 @@ export default function Profile() {
   });
 
   return (
-    <Container id="profile">
-      <Row className=" justify-content-center">    
-        <Card style={{ width: '18rem' }}>
+      <Row className=" justify-content-center" id="profile">    
+        <Card className="col">
           <Card.Img variant="top" src={audioWave} />
           <Card.Body>
             <Card.Title>Service Name</Card.Title>
@@ -30,8 +29,56 @@ export default function Profile() {
           </div>
           </Card.Body>
         </Card>
+        <Card className="col">
+          <Card.Img variant="top" src={audioWave} />
+          <Card.Body>
+            <Card.Title>Service Name</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          </Card.Body>
+          <Card.Body>
+          <div className="row justify-content-between">
+          <Button className="col-4" as="input" type="button" value="Edit" />{' '}
+          <Button className="col-4 bg-danger" as="input" type="button" value="Delete"/>{' '}
+          </div>
+          </Card.Body>
+        </Card>
+        <Card className="col">
+          <Card.Img variant="top" src={audioWave} />
+          <Card.Body>
+            <Card.Title>Service Name</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          </Card.Body>
+          <Card.Body>
+          <div className="row justify-content-between">
+          <Button className="col-4" as="input" type="button" value="Edit" />{' '}
+          <Button className="col-4 bg-danger" as="input" type="button" value="Delete"/>{' '}
+          </div>
+          </Card.Body>
+        </Card>
+        <Card className="col">
+          <Card.Img variant="top" src={audioWave} />
+          <Card.Body>
+            <Card.Title>Service Name</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          </Card.Body>
+          <Card.Body>
+          <div className="row justify-content-between">
+          <Button className="col-4" as="input" type="button" value="Edit" />{' '}
+          <Button className="col-4 bg-danger" as="input" type="button" value="Delete"/>{' '}
+          </div>
+          </Card.Body>
+        </Card>
+        
       </Row>
-  </Container>
   );
     
 }
