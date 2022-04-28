7aa87c36989731bd32015d703cbaa76696ff033c
Andreas Kaiafas <akaiafas526@gmail.com>
2022-04-28 14:10:41 -0400

    filter for category nav is functional

--- client/src/components/JobCard/JobCard.js
+++ client/src/components/JobCard/JobCard.js
@@ -1,18 +1,16 @@
-import './index.css'
+import "./index.css";
 import React from "react";
-import "./index.css"
+import "./index.css";
 import { Link } from "react-router-dom";
 
-export default function JobCard({job}) {
+export default function JobCard({ job }) {
   return (
     <Link className="col-3 p-2" to="/job">
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">{job.name}</h5>
           <h6 className="card-subtitle mb-2">${job.price}</h6>
-          <p className="card-text">
-            {job.description}
-          </p>
+          <p className="card-text">{job.description}</p>
         </div>
       </div>
     </Link>
@@ -24,5 +22,3 @@ export default function JobCard({job}) {
 //     name: "student"
 //   }
 // }
-
-// console.log(props.job.name)
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,6 +2,6 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import JobCard from "./JobCard/JobCard";
-import Listing from "./Listing/Listing";
+import Listing from "./Listing";
 
 export { Footer, Header, JobCard, Listing };
