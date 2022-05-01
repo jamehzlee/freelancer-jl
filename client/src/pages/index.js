5fa88a91945b46bd51727d8e3c03e1406301c67a
Jamehzz <probableeme@gmail.com>
2022-05-01 12:11:22 -0400

    updated to include Edit page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,5 +4,6 @@ import Login from "./Login/Login";
 import Signup from "./Signup/Signup";
 import Profile from "./Profile/Profile";
 import PostJob from "./PostJob/PostJob"
+import Edit from "./Edit/Edit"
 
-export { Home, Job, Login, Signup, Profile, PostJob};
+export { Home, Job, Login, Signup, Profile, PostJob, Edit };
